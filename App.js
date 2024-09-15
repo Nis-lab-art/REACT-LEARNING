0b6a55e0c5cba280a23739658b36e5070cc58ff5
Nishan Giri <nishan.giri789@gmail.com>
2024-09-15 11:38:57 +0530

    Using react

--- App.js
+++ App.js
@@ -1,23 +1,20 @@
-const header = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello From React!"
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const Title = () => (
+  <h1 className="heading" tabIndex={1}>
+    Hello there!
+  </h1>
 );
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" }, [
-      React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am a H1 inside a parent and child"),
-    React.createElement("h1", {}, "I am a H2 inside a parent and child"),
-  ]),
-    React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I am a H1 inside a parent and child2"),
-    React.createElement("h1", {}, "I am a H2 inside a parent and child2"),
-  ])
-  ]
-);
+const Heading = () => {
+  return (
+    <div id="container">
+      <Title />
+      <h1>I am a heading</h1>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
-console.log(parent);
+root.render(<Heading />);
