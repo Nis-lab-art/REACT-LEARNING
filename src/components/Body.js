01d6d3ff49a1aafff87d04122f8b3127c75024d0
Nishan Giri <nishan.giri789@gmail.com>
2024-09-19 00:07:47 +0530

    Added new filter and search feature

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,12 +4,23 @@ import Shimmer from "./Shimmer";
 
 const Body = () => {
   const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  const [productCat, setProductCat] = useState("");
+  const [searchValue, setSearchValue] = useState("");
+
+  const handleProductCategory = (e) => {
+    let newCategory = e.target.value;
+    setProductCat(newCategory);
+    let res = products.filter((item) => item.productCategory === newCategory);
+    setFilteredProducts(res);
+  };
 
   const getData = async () => {
     const response = await fetch("https://dummyapi.online/api/products");
     const json = await response.json();
     console.log(json);
     setProducts(json);
+    setFilteredProducts(json);
   };
 
   useEffect(() => {
@@ -18,21 +29,60 @@ const Body = () => {
 
   if (products.length === 0) return <Shimmer />;
 
-  return (
+  return products.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body-container">
-      <div className="filter-btn">
-        <button
-          className="top-res-btn"
-          onClick={() => {
-            let topRes = products.filter((item) => item.basePrice <= 999);
-            setProducts(topRes);
-          }}
-        >
-          Cheap Product
-        </button>
+      <div className="filter-options">
+        <div className="search-filter">
+          <input
+            type="text"
+            className="search-input"
+            value={searchValue}
+            onChange={(e) => {
+              setSearchValue(e.target.value);
+            }}
+          />
+          <button
+            onClick={() => {
+              const res = products.filter((item) =>
+                item?.name?.toLowerCase().includes(searchValue?.toLowerCase())
+              );
+              setFilteredProducts(res);
+            }}
+          >
+            Search
+          </button>
+        </div>
+        <div className="category-filter">
+          {" "}
+          <label>Choose a car:</label>
+          <select
+            name="cars"
+            className="select"
+            onChange={handleProductCategory}
+          >
+            <option value="">Select a category...</option>
+            <option value="Smartphone">Smartphone</option>
+            <option value="Tablet">Tablet</option>
+            <option value="Laptop">Laptop</option>
+          </select>
+        </div>
+        <div>
+          {" "}
+          <button
+            className="top-res-btn"
+            onClick={() => {
+              let topRes = products.filter((item) => item.basePrice <= 999);
+              setFilteredProducts(topRes);
+            }}
+          >
+            Cheap Product
+          </button>
+        </div>
       </div>
       <div className="res-container">
-        {products.map((item) => (
+        {filteredProducts.map((item) => (
           <ProductCard key={item.id} resData={item} />
         ))}
       </div>
--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,13 +1,20 @@
 const ProductCard = (props) => {
   const { resData } = props;
-  const { name, brand, description, basePrice, thumbnailImage } = resData;
+  const {
+    name,
+    brand,
+    description,
+    basePrice,
+    thumbnailImage,
+    productCategory,
+  } = resData;
   return (
     <div className="res-card">
       <img className="res-image" alt="res-image" src={thumbnailImage} />
-      <h3>{name}</h3>
+      <h4>{name}</h4>
       <h4>{brand}</h4>
-      <h4>{description}</h4>
-      <h4>Rs. {basePrice}</h4>
+      <p>{description}</p>
+      <p>Rs. {basePrice}</p>
     </div>
   );
 };
