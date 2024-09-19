dcfbf4dcdaf583658632b6eb4b524aff0d4b87ea
Nishan Giri <nishan.giri789@gmail.com>
2024-09-20 00:19:11 +0530

    Added some content in about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,39 @@
-import React from "react";
+import { Component } from "react";
 
-const About = () => {
-  return (
-    <div>
-      <h1>About page</h1>
-    </div>
-  );
-};
+class About extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 10,
+    };
+    console.log("Constructor is callled");
+  }
+
+  componentDidMount() {
+    console.log("Comonent did mount");
+  }
+
+  render() {
+    console.log("Rendered is called");
+    const { count } = this.state;
+
+    return (
+      <div>
+        {" "}
+        <h1>About</h1>
+        <h2>{count}</h2>
+        <button
+          onClick={() => {
+            this.setState({
+              count: count + 1,
+            });
+          }}
+        >
+          Counter
+        </button>
+      </div>
+    );
+  }
+}
 
 export default About;
